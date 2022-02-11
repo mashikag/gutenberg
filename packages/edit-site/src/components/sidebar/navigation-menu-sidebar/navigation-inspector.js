5314dce99827ea425d584802faa5f258caf00aae
Kerry Liu <kerryliu@gmail.com>
2022-02-11 09:11:40 -0800

    turn on block movers

--- packages/edit-site/src/components/sidebar/navigation-menu-sidebar/navigation-inspector.js
+++ packages/edit-site/src/components/sidebar/navigation-menu-sidebar/navigation-inspector.js
@@ -100,6 +100,7 @@ export default function NavigationInspector() {
 				<ListView
 					blocks={ blocks }
 					showNestedBlocks
+					showBlockMovers
 					__experimentalFeatures
 					__experimentalPersistentListViewFeatures
 				/>
